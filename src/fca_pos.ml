177d6c1735824134908eb3aa239c54074d9c399a
Ildar Mulyukov <ildar@altlinux.ru>
2023-08-28 01:06:27 +0600

    Fca.Pos.is_valid: improve checks

--- src/fca_pos.ml
+++ src/fca_pos.ml
@@ -2,8 +2,10 @@ open Base
 open Base.String
 open Fca_types
 
+(* https://en.wikipedia.org/wiki/Partially_ordered_set *)
 let is_valid = fun a_pos ->
   let { elems=elems; rels=rels } = a_pos in
+  (* check relations' elements exist *)
   List.fold
     rels
     ~init: true
@@ -13,4 +15,13 @@ let is_valid = fun a_pos ->
         acc &&
           List.exists elems ~f:(fun e-> a = e) &&
           List.exists elems ~f:(fun e-> b = e)
+    ) &&
+  (* check reflexivity relations not included *)
+  List.fold
+    rels
+    ~init: true
+    ~f:(
+      fun acc rel ->
+        let (a,b) = rel in
+        acc && a <> b
     )
--- test/pos_test.ml
+++ test/pos_test.ml
@@ -3,13 +3,18 @@ open OUnit2
 open Fca
 
 let tests = "FCA module POS function" >::: [
-    "can check consistency"  >:: (fun _ ->
-        let a_pos = {
+    "can check consistency" >:: (fun _ ->
+        let a_pos1 = {
           elems=[];
           rels=[ ("1","1"); ];
         } in
-        assert_bool "Pos.is_valid false positive" @@ not @@ Pos.is_valid a_pos;
-      );
+        assert_bool "Pos.is_valid false positive" @@ not @@ Pos.is_valid a_pos1;
+        let a_pos2 = {
+          elems=["0";"1"];
+          rels=[ ("1","1"); ];
+        } in
+        assert_bool "Pos.is_valid shouldn't have a<=a relations" @@
+          not @@ Pos.is_valid a_pos2;
   ]
 
 let _ = run_test_tt_main tests
